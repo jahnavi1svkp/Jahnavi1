58fa7e54cacc70fe8ee3f5e5dc30760a974402f0
Dell <Dell@DESKTOP-V3B8B37>
2025-04-04 16:24:22 +0530

    commit1

---
+++ svkp/src/basicsofjava/BreakDemo.java
@@ -0,0 +1,16 @@
+package basicsofjava;
+
+public class BreakDemo {
+	public static void main(String[] args) {
+		for(int i=1;i<100;i++)
+		{
+			if(i>50)
+			{
+				break;
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
---
+++ svkp/src/basicsofjava/SimpleIfDemo.java
@@ -0,0 +1,16 @@
+package basicsofjava;
+
+public class SimpleIfDemo {
+	public static void main(String[] args) {
+		int age=11;
+		if(age>18)
+		{
+			System.out.println("you are eligible");
+		}
+		else
+		{
+			System.out.println("you are not eligible");
+		}
+	}
+
+}
